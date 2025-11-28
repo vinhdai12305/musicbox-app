63946e848157f3723722cf0392dacf8dcef9d035
vinhdai12305 <tvdaii12032005@gmail.com>
2025-11-28 15:15:25 +0700

    Clean .DS_Store and update .gitignore

---
+++ app/src/main/java/com/midterm/musicbox/PlayerActivity.java
@@ -0,0 +1,14 @@
+package com.midterm.musicbox;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+    }
+}
